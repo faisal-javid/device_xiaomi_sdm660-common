e294114c337e3f7802994527f411fcd8dce56410
ParasBansal25 <parasbansal1331@gmail.com>
2021-08-28 13:13:24 +0530

    Declare Mi A2 specs

--- sdm660.mk
+++ sdm660.mk
@@ -385,7 +385,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(COMMON_PATH)/overlay
+    $(COMMON_PATH)/overlay \
+    $(COMMON_PATH)/overlay-xd
 
 # RRO configuration
 TARGET_USES_RRO := true
