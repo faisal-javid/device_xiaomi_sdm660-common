31c1893a616b9829a0485f07881f9f879493f336
faisal-javid <fjavid68@gmail.com>
2021-12-29 03:20:38 +0530

    Remove references for antradio_library

--- sdm660.mk
+++ sdm660.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 
 # ANT Permission
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    AntHalService
 
 # AuthSecret
 PRODUCT_PACKAGES += \
